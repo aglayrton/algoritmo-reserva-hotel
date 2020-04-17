96714a7e8edfa9c5fba49aaf06d68689d10a8c26
Aglayrton Julião <aglayrtonjuliao@gmail.com>
2020-04-16 23:10:22 -0300

    Projeto criado

---
+++ src/tecnologia/Teste.java
@@ -0,0 +1,16 @@
+package tecnologia;
+
+import java.util.Scanner;
+
+public class Teste {
+
+	
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 4; i++) {
+			int j = i;
+		}
+		
+	}
+
+}
